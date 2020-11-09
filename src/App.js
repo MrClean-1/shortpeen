9a7ef97b905f9413dc33559230f4f8e829614364
MrClean-1 <65871854+MrClean-1@users.noreply.github.com>
2020-11-08 21:03:10 -0700

    App changes in url-shortner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import LinkForm from "./LinkForm";
+import SlugForm from "./SlugForm";
+
+/* TODO
+Create a global submission button
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <LinkForm />
+        <SlugForm />
+      </div>
+      );
 }
 
 export default App;
