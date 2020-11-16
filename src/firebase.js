b530ac72bde0625c5f797b7e8f70c207f15abfc1
MrClean-1 <Cjdrewes@gmail.com>
2020-11-15 22:30:07 -0700

    Firebase change

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import app from "@firebase/app";
+
+const fire = app.initializeApp({
+    apiKey: "AIzaSyCu0QOH90LY0nLWf7r3PoHRF1f1rgj87bQ",
+    authDomain: "shortpeen-6d06d.firebaseapp.com",
+    databaseURL: "https://shortpeen-6d06d.firebaseio.com",
+    projectId: "shortpeen-6d06d",
+    storageBucket: "shortpeen-6d06d.appspot.com",
+    messagingSenderId: "670603460985",
+    appId: "1:670603460985:web:6ebdec4338e66a2a85afa8",
+    measurementId: "G-QS0HBKPZBL"
+});
+
+export default fire;
